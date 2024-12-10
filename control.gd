db1038f77ca736845c9bf67e6a3bc1b675831b17
Sander C. Braathen <sanderbraathen67@gmail.com>
2024-12-10 14:13:26 +0100

    Update control.gd

--- control.gd
+++ control.gd
@@ -8,8 +8,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	$MarginContainer/VBoxContainer/money.text = "Money: " + str(GameManager.money).pad_decimals(0)
+	$MarginContainer/VBoxContainer/customer.text = "Customers: " + str(GameManager.customer).pad_decimals(0)
+	$money_s.text = "Money/s: " + str(GameManager.money_s).pad_decimals(1)
+	$customer_s.text = "Customers/s: " + str(GameManager.customer_s).pad_decimals(1)
+	
 
 func _on_texture_button_pressed() -> void:
-	GameManager.money += 1
+	GameManager.money += GameManager.click_amount
+
+
+func _on_timer_timeout() -> void:
+	var money:float = GameManager.money_s * 0.1
+	var customer:float = GameManager.customer_s * 0.1
+	
+	GameManager.money += money
+	GameManager.customer += customer
