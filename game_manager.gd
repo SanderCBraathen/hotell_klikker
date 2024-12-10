c93c4043d18c7cd907f37e9dc0bd049b1f8419a6
Sandify <sandergithub@gmail.com>
2024-12-10 20:44:01 +0100

    Update game_manager.gd

--- game_manager.gd
+++ game_manager.gd
@@ -6,9 +6,25 @@ var customer:float = 0
 var money_s:float = 0
 var customer_s:float = 0
 
-var click_amount:float = 1
+var click_amount:float = 5
 
 
 var upgrade1_cost = 25
-var upgrade1_money = 0.5
+var upgrade1_money = 0.1
+var upgrade1_customer = 0.05
 var upgrade1_amount = 0
+
+var upgrade2_cost = 100
+var upgrade2_money = 1
+var upgrade2_customer = 0.1
+var upgrade2_amount = 0
+
+var upgrade3_cost = 2500
+var upgrade3_money = 8
+var upgrade3_customer = 0.5
+var upgrade3_amount = 0
+
+var upgrade4_cost = 15000
+var upgrade4_money = 30
+var upgrade4_customer = 3
+var upgrade4_amount = 0
