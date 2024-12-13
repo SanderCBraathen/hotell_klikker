1f6078d7e59632e041608ecd06ff760548d81dc3
Sander C. Braathen <sanderbraathen67@gmail.com>
2024-12-13 09:15:23 +0100

    Update upgrade_1.gd

--- upgrade_1.gd
+++ upgrade_1.gd
@@ -12,6 +12,10 @@ func _process(delta: float) -> void:
 	else:
 		self.disabled = false
 	
+	if GameManager.upgrade1_cost >= 1000:
+		$cost.text = str(GameManager.upgrade1_cost / 1000).pad_decimals(1) + "K"
+	else:
+		$cost.text = str(GameManager.upgrade1_cost).pad_decimals(0)
 	
 	$cost.text = str(GameManager.upgrade1_cost).pad_decimals(0)
 	$amount.text = str(GameManager.upgrade1_amount)
